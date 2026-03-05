d9eb4e359c892e49b3fc5da343c8747b9c4b90c8
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2026-03-06 06:07:31 +0700

    Update VoxelHashMap.cpp

--- cpp/kiss_icp/core/VoxelHashMap.cpp
+++ cpp/kiss_icp/core/VoxelHashMap.cpp
@@ -1,25 +1,3 @@
-// MIT License
-//
-// Copyright (c) 2022 Ignacio Vizzo, Tiziano Guadagnino, Benedikt Mersch, Cyrill
-// Stachniss.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
 #include "VoxelHashMap.hpp"
 
 #include <Eigen/Core>
@@ -128,6 +106,34 @@ void VoxelHashMap::RemovePointsFarFromLocation(const Eigen::Vector3d &origin) {
         } else {
             ++it;
         }
-    }
+    } 
 }
+std::tuple<Eigen::Vector3d, std::vector<Eigen::Vector3d>, double>
+VoxelHashMap::GetClosestNeighborAndNeighbors(const Eigen::Vector3d &query) const {
+     const auto &voxel = PointToVoxel(query, voxel_size_);
+     Eigen::Vector3d closest_neighbor = Eigen::Vector3d::Zero();
+     double closest_distance = std::numeric_limits<double>::max();
+     std::vector<Eigen::Vector3d> neighbors;
+
+     for (const auto &voxel_shift : voxel_shifts) {
+         const auto &query_voxel = voxel + voxel_shift;
+         auto search = map_.find(query_voxel);
+         if (search != map_.end()) {
+             const auto &points = search.value();
+             neighbors.insert(neighbors.end(), points.cbegin(), points.cend());
+             const Eigen::Vector3d &neighbor = *std::min_element(
+                 points.cbegin(), points.cend(),
+                 [&](const auto &lhs, const auto &rhs) {
+                     return (lhs - query).norm() < (rhs - query).norm();
+                 });
+             double distance = (neighbor - query).norm();
+             if (distance < closest_distance) {
+                 closest_neighbor = neighbor;
+                 closest_distance = distance;
+             }
+         }
+     }
+     return std::make_tuple(closest_neighbor, neighbors, closest_distance);
+ }
+
 }  // namespace kiss_icp
